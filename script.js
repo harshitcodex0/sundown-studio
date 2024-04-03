def1958a924bfe6015235da06f5bb22a973d4907
harshitcodex0 <harshit456987@hotmail.com>
2024-04-03 21:58:09 +0530

    working on page3 effects

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const scroll = new LocomotiveScroll({
   smooth: true,
   lerp: 0.05,
 });
+
+// Fixed Image Code
+var elemC = document.querySelector("#elem-container")
+var fixed = document.querySelector("#fixed-image")
+elemC.addEventListener("mouseenter", function(){
+  fixed.style.display = "block"
+})
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
